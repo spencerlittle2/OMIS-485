f1842362e2afe23a6831dd34fdcf95ac655ed8c1
spencerlittle2 <47013964+spencerlittle2@users.noreply.github.com>
2019-03-02 00:48:24 -0600

    Add files via upload

--- email_list.js
+++ email_list.js
@@ -8,35 +8,49 @@ var joinList = function() {
     var emailAddress1 = $("email_address1").value;
     var emailAddress2 = $("email_address2").value;
     var firstName = $("first_name").value;
-    var errorMessage = "";
+    // change to Boolean
+    var isValid = true;
 
     // validate the entries
     if (emailAddress1 == "") {
-    	errorMessage = "First email address entry required";
-        $("email_address1").focus();
-    } else if (emailAddress2 == "") {
-    	errorMessage = "Second email address entry required";
-    	$("email_address2").focus();
-    } else if (emailAddress2 != emailAddress1) {
-    	errorMessage = "Email address entries must match";
-    	$("email_address2").focus();
-    } else if (firstName == "") {
-    	errorMessage = "First name entry required";
-    	$("first_name").focus();
+        $("email_address1").nextElementSibling.firstChild.nodeValue
+            = "The first email_address is required.";
+            isValid = false;   
+         }
+
+    else { 
+    $("email_address1").nextElementSibling.firstChild.nodeValue = ""; 
     }
+    if (emailAddress2 == "") {
+    $("email_address2").nextElementSibling.firstChild.nodeValue 
+    = "The second email_address is required.";
+        isValid = false;
+    }
+    else if (emailAddress2 != emailAddress1) {
+        $("email_address2").nextElementSibling.firstChild.nodeValue 
+        = "The emailaddresses must match.";
+        isValid = false;
+    }
+    else { 
+        $("email_address2").nextElementSibling.firstChild.nodeValue = "";
+
+    }
+    if (firstName == "") {
+        $("first_name").nextElementSibling.firstChild.nodeValue 
+         = "First name entry is required.";
+         isValid = false;
+    }
+
+
 
     // submit the form if all entries are valid
     // otherwise, display an error message
-    if (errorMessage == "") {
-    //In order to fix this error I needed to add one more = sign 
+    if (isValid) {
         $("email_form").submit(); 
-    } else {
-    	alert(errorMessage);
     }
 };
 
 window.onload = function() {
-    //I changed it to $("join_list").onclick = joinList;
     $("join_list").onclick = joinList;
     $("email_address1").focus();
 };
