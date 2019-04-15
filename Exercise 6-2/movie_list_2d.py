7c0fbd1d5fb5cfdc5d94aefeaaa273f8b3fd8b2a
spencerlittle2 <spencerlittle2@yahoo.com>
2019-04-14 22:00:47 -0500

    good to go

--- Exercise 6-2/movie_list_2d.py
+++ Exercise 6-2/movie_list_2d.py
@@ -14,12 +14,10 @@ def list(movie_list):
         print("There are no movies in the list.\n")
         return
     else:
-        i = 0
+        i = 1
         for movie in movie_list:
             row = movie
-            print(str(i+1) + ". " + row[0] 
-                + " (" + str(row[1]) + ")"
-                + " @ " + str(row[2]))
+            print(str(i) + ". " + row[0] + " (" + str(row[1]) + ")")
             i += 1
         print()
 
